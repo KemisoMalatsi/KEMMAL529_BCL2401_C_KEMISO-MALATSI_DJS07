c029e58c674eaaf9258a29f964e0811ff0124d66
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-05-30 17:00:41 +0200

    1. Set up the text inputs to save to the `topText` and `bottomText` state variables. 2. Make the text on the image be saved to state.

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -23,6 +23,16 @@ export default function Meme () {
 
     }
 
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
+
+
     return (
         <main>
             <div className="form">
@@ -31,10 +41,17 @@ export default function Meme () {
                     type= "text"
                     placeholder="Top text" 
                     className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
-                <input type= "text"
+                <input 
+                    type= "text"
                     placeholder="bottom text" 
                     className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button 
                     className="form--button"
@@ -43,7 +60,11 @@ export default function Meme () {
                     Get a new meme image
                 </button>
             </div>
-            <img src={meme.randomImage} className="meme--image"/>
+            <div>
+                <img src={meme.randomImage} className="meme--image"/>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }

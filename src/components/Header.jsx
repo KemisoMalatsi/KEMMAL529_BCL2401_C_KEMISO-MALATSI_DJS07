35f5e3328b209d988f2d175d5333161674fdf658
KemisoMalatsi <kemiso.malatsi@gmail.com>
2024-05-30 12:52:27 +0200

    Created a jsx file for Header and then exported it to the App.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+function Header () {
+    return (
+        <header className="header">
+            <img src ="" />
+            <h2> Header Components</h2>
+            <h4>React Course - Project 3</h4>
+        </header>
+    )
+}
+
+export default Header;
